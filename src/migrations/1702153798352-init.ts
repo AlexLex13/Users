5531f6341ab712bf4d4010bd45df1e6c300854e1
Alexey <suxovarov.alexey@gmail.com>
2023-12-10 20:37:42 +0300

    Edit migration script

--- src/migrations/1702153798352-init.ts
+++ src/migrations/1702153798352-init.ts
@@ -6,7 +6,7 @@ export class Init1702153798352 implements MigrationInterface {
         await queryRunner.query(
       ` 
           --Table Definition
-          CREATE TABLE "users"  (
+          CREATE TABLE IF NOT EXISTS "users"  (
             "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
             "firstName" character varying NOT NULL,
             "lastName" character varying,
@@ -14,14 +14,17 @@ export class Init1702153798352 implements MigrationInterface {
             "password" character varying NOT NULL,
             "image"  character varying,
             "pdf" bytea,
-            CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id");
+            CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id")
           )
-          CREATE UNIQUE INDEX email_idx ON users (email);
           `
         )
+
+        await queryRunner.query(`CREATE UNIQUE INDEX IF NOT EXISTS email_idx ON users (email)`)
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE IF EXISTS users`)
+        await queryRunner.query(`DROP INDEX IF EXISTS email_idx`)
     }
 
 }
